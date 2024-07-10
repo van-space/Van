24e324a1ceba6cd30500383048b136bfeab710f2
huangyan321 <1604549268@qq.com>
2024-07-10 11:27:05 +0800

    feat: theme color

---
+++ src/hooks/data/use-aggregation.ts
@@ -0,0 +1,8 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { aggregation } from '../../queries/definition/aggregation'
+export const useAggregation = () => {
+  return useQuery({
+    ...aggregation.root(),
+  })
+}
