20a20a1fa7797007cf723c5ee60f7b7a6f0cee54
huangyan321 <1604549268@qq.com>
2024-07-11 11:23:37 +0800

    perf: re-reduce router change re-render

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,20 +1,35 @@
 'use client'
 
+import { useRef } from 'react'
 import { useParams } from 'next/navigation'
 
 import { Toc, TocAutoScroll } from '~/components/widgets/toc'
 import { useNoteByNidQuery } from '~/hooks/data/use-note'
 import { PageDataHolder } from '~/lib/page-holder'
 import { ArticleElementProvider } from '~/providers/article/article-element-provider'
+import { useSetNoteId } from '~/providers/note/note-id-provider'
 import { NoteLayoutRightSidePortal } from '~/providers/note/right-side-provider'
 import { parseMarkdown } from '~/remark'
+import { isClientSide } from '~/utils/env'
 
 const PageImpl = () => {
+  console.log('render');
+  
   const { id } = useParams() as { id: string }
   const { data } = useNoteByNidQuery(id)
 
   const mardownResult = parseMarkdown(data?.data?.text ?? '')
 
+  // Why do this, I mean why do set NoteId to context, don't use `useParams().id` for children components.
+  // Because any router params or query changes, will cause components that use `useParams()` hook, this hook is a context hook,
+  // For example, `ComA` use `useParams()` just want to get value `id`,
+  // but if router params or query changes `page` params, will cause `CompA` re - render.
+  const setNoteId = useSetNoteId()
+  const onceRef = useRef(false)
+  if (isClientSide() && !onceRef.current) {
+    onceRef.current = true
+    setNoteId(id)
+  }
   return (
     <article className="prose">
       <header>
